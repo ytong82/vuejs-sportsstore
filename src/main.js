82146b8c30a3a9733cb4030fd05611bbcd772f78
David Tong <ytong@ebay.com>
2020-06-01 03:43:45 -0500

    Restful API

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.min.css"
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
