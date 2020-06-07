aae0cbf2a8e1f5e9ca9836e021d67f398c9872eb
David Tong <ytong@ebay.com>
2020-06-07 02:29:44 -0500

    Bugfix

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,14 @@ import { mapActions } from "vuex";
 export default {
   name: 'App',
   methods: {
-    ...mapActions(["getData"]),
-    //initializeCart: "cart/initializeCart"
+    ...mapActions({
+      getData: "getData",
+      initializeCart: "cart/initializeCart"
+    })
   },
   created() {
     this.getData();
-    //this.initializeCart(this.$store);
+    this.initializeCart(this.$store);
   }
 }
 </script>
