de324de550e387f694dcaefa0daf888e3b8a3cbf
David Tong <ytong@ebay.com>
2020-06-07 02:22:48 -0500

    Bugfix

--- src/store/index.js
+++ src/store/index.js
@@ -30,7 +30,7 @@ export default new Vuex.Store({
       let index = (state.currentPage - 1) * state.pageSize;
       return getters.productsFilteredByCategory.slice(index, index + state.pageSize);
     },
-    pageCount: (state, getters) => Math.ceil(getters.processedProducts / state.pageSize),
+    pageCount: (state, getters) => Math.ceil(getters.productsFilteredByCategory.length / state.pageSize),
     categories: state => ["All", ...new Set(state.products.map(p => p.category).sort())]
   },
   mutations: {
